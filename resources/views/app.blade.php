146feec874e9611961a7d1fc9f53667b5ea838b1
Anggit Ari Utomo <pucukpisang97@gmail.com>
2026-08-16 22:08:12 +0700

    fix: :bug: fix manifest build

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>{{ config('app.name') ?? "Stock Opname Inertia" }}</title>
-	@vite(['resources/js/app.js', 'resources/css/app.css'])
+	@vite(['resources/js/app.ts', 'resources/css/app.css'])
 	@fonts
 	@inertiaHead
 </head>
